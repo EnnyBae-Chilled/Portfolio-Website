47b31fedba2e3660b220640a24679fbe1fd7d5dd
Eniola Olawumi Farinde <144468968+EnnyBae-Chilled@users.noreply.github.com>
2024-06-16 22:26:55 -0500

    Add files via upload

---
+++ personal.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text', {
+    strings: ['Software Developer', 'UI/UX Designer', 'Front-End Developer', 'Computer Science Enthusiast'],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
